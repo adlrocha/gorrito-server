0f648ac800002290dfda90e9ca1d5ee8ce428992
adlrocha <alfon.rocha@gmail.com>
2019-09-27 19:11:00 +0200

    Real time shitty version

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var path = require('path')
+ 
+var bodyParser = require('body-parser');
+
+var storage = {"alarma": "No Alarm"}
+
+app.use(bodyParser.json());
+
+// app.get("/"), function (req, res) {
+//   console.log("Serving index.html...")
+//   res.sendfile(html_dir + 'index.html');
+// }
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/data', function (req, res) {
+  console.log("Requesting alarm status. GET /")
+  res.send(storage);
+});
+
+// POST method route
+app.post('/send', function (req, res) {
+    console.log("Received data from sensor")
+    console.log(req.body)
+    storage = req.body
+    // if (req.body.alarma != undefined) {
+      res.send({"Received": "OK"})
+    // } else {
+    //   res.send({"error": "Malformed request"})
+    // }
+    
+  })
+ 
+app.listen(8180, function () {
+  console.log('App listening on port 8180!');
+});
