e257e4c24bb9309b9cef24aeb7443a568fd2ccb5
adlrocha <alfon.rocha@gmail.com>
2019-10-03 20:36:50 +0200

    Added CORS support

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.use(bodyParser.json());
 //   res.sendfile(html_dir + 'index.html');
 // }
 
+app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/data', function (req, res) {
